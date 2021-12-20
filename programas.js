a61ff870ab08171bad3359d9fd71b71da391c510
Gabriel Christo <christo@dcc.ufrj.br>
2021-12-20 01:49:31 -0300

    timesheetjs and calendarjs; home page with bootstrap navbar; added some programs status

---
+++ programas.js
@@ -0,0 +1,73 @@
+var programas = [
+	// 2003 Completo + Incompleto
+    { title: "Incompleto (Programa 1)", from: "2003-09-28", url: "https://www.google.com" },
+	{ title: "Incompleto", from: "2003-11-09", url: "https://www.google.com" },
+
+	// 2004 Completo + Incompleto
+	{ title: "Completo", from: "2004-02-15", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-02-22", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-03-07", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-03-14", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-03-28", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-04-04", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-04-11", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-04-18", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-04-25", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-05-02", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-05-16", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-05-23", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-05-30", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-06-06", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-06-13", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-06-20", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-06-27", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-07-11", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-08-01", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-08-08", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-08-22", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-09-05", url: "https://www.google.com" },
+	{ title: "Completo (Especial 1 Ano)", from: "2004-10-03", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-12-12", url: "https://www.google.com" },
+	{ title: "Completo", from: "2004-12-19", url: "https://www.google.com" },
+
+	// 2005 Completo + Incompleto
+	{ title: "Completo", from: "2005-01-02", url: "https://www.google.com" },
+	{ title: "Completo", from: "2005-01-09", url: "https://www.google.com" },
+	{ title: "Completo", from: "2005-01-16", url: "https://www.google.com" },
+	{ title: "Completo", from: "2005-04-03", url: "https://www.google.com" },
+	{ title: "Completo (Programa 100)", from: "2005-08-17", url: "https://www.google.com" }, // quarta
+	{ title: "Incompleto", from: "2005-10-16", url: "https://www.google.com" },
+
+	// 2006 Completo + Incompleto
+	{ title: "Completo (Retrospectiva 2005)", from: "2006-01-01", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-04-09", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-04-30", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-05-07", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-05-21", url: "https://www.google.com" },
+	{ title: "Incompleto", from: "2006-07-30", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-10-01", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-10-08", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-11-05", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-11-19", url: "https://www.google.com" },
+	{ title: "Incompleto", from: "2006-11-26", url: "https://www.google.com" },
+	{ title: "Completo", from: "2006-12-24", url: "https://www.google.com" },
+
+	// 2007 Completo + Incompleto
+	{ title: "Completo", from: "2007-01-28", url: "https://www.google.com" },
+	{ title: "Completo", from: "2007-04-27", url: "https://www.google.com" }, // sexta
+	{ title: "Completo", from: "2007-05-13", url: "https://www.google.com" },
+	{ title: "Completo (Bizarro 200)", from: "2007-07-29", url: "https://www.google.com" },
+	{ title: "Completo", from: "2007-10-27", url: "https://www.google.com" },
+	{ title: "Completo", from: "2007-11-02", url: "https://www.google.com" }, // sexta
+
+	// 2008 Completo + Incompleto
+
+	// 2009 Completo + Incompleto
+
+	// 2010 Completo + Incompleto
+
+	// 2011 Completo + Incompleto
+
+	// 2012 Completo + Incompleto
+	{ title: "Completo (Retrospectiva 2011)", from: "2012-01-01", url: "https://www.google.com" },
+];
